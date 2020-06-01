ef18f6f9d59eac0ca4db930723662e442c8d5e3f
majorMobilych <godmihovelcplus@gmail.com>
2020-06-01 21:14:18 +0300

    Refactor

--- src/main/java/solutions/SolutionsInterface.java
+++ src/main/java/solutions/SolutionsInterface.java
@@ -3,11 +3,27 @@ package solutions;
 import java.util.List;
 
 public interface SolutionsInterface {
+
+    /**
+     * Watch first task;
+     */
     List<String> firstTaskEasy(String text);
+
     List<String> firstTaskMedium(String text);
+
     List<String> firstTaskHard(String text);
+
+    /**
+     * Watch second task;
+     */
     List<String> secondTaskEasy(String text);
+
     List<String> secondTaskHard(String text);
+
+    /**
+     * Watch third task;
+     */
     List<String> thirdTaskEasy(String text);
+
     List<String> thirdTaskHard(String text);
 }
